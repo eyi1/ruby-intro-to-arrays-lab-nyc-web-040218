decf8ebb6b69a2d54dde7504a401a75e1a990f41
eyi1 <ej7729@gmail.com>
2018-03-27 23:29:49 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -12,4 +12,20 @@ end
 
 def third_element (array)
 array[2]
+end
+
+def last_element (array)
+  array.last
+end
+
+def first_element_with_array_methods(array)
+  array.first
+end 
+
+def last_element_with_array_methods (array)
+  array.last
+end
+
+def length_of_array (array)
+  array.length
 end 
