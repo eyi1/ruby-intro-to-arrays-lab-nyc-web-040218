034c5efa88d02fa9ad1a00a501140dbdfb5e6b22
eyi1 <ej7729@gmail.com>
2018-03-27 23:29:54 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -20,7 +20,7 @@ end
 
 def first_element_with_array_methods(array)
   array.first
-end 
+end
 
 def last_element_with_array_methods (array)
   array.last
@@ -28,4 +28,4 @@ end
 
 def length_of_array (array)
   array.length
-end 
+end
